2def04be1d47e310389e65093eabce96d880ef74
skyse <1052754704@qq.com>
2024-07-01 21:04:15 +0800

    add func ShowTip

--- Bouncing Donut/Assets/Script/UScore.cs
+++ Bouncing Donut/Assets/Script/UScore.cs
@@ -18,32 +18,24 @@ namespace Script
 
         private void OnGUI()
         {
+            void ShowTip(int boxFontSize, string tip)
+            {
+                GUI.skin.box.fontSize = boxFontSize;
+                GUI.Box(new Rect(Screen.width/2 - 200, Screen.height/2 - 50, 400, 100), tip);
+            }
+            
             GUI.skin.box.fontSize = 30;
             GUI.Box(new Rect(20,20,200,50), "Score: " + Score);
-            
-            GUI.Box(new Rect(Screen.width - 200, 20, 200, 50),
-                "Level " + UGameState.Level);
+            GUI.Box(new Rect(Screen.width - 200, 20, 200, 50), "Level " + UGameState.Level);
             
             if (UGameState.IsGameOver())
             {
-                GUI.skin.box.fontSize = 60;
-                GUI.Box(new Rect(
-                    Screen.width/2 - 200,
-                    Screen.height/2 - 50,
-                    400,
-                    100),
-                    "Game Over");
+                ShowTip(60, "Game Over");
             }
 
             if (UGameState.IsLevelComplete())
             {
-                GUI.skin.box.fontSize = 40;
-                GUI.Box(new Rect(
-                        Screen.width/2 - 250,
-                        Screen.height/2 - 50,
-                        400,
-                        100),
-                    "Level Complete");
+                ShowTip(40, "Level Complete");
             }
         }
     }
